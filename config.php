3727cf715dd9dda06a6af4c521e73dc8d46a918c
Andi Irzan <serarechannel@gmail.com>
2024-07-10 18:35:43 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-const GROQ_API_KEY = ''; // Your Groq API Key
+const GROQ_API_KEY = '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'; // Your Groq API Key
 const GROQ_MODEL = 'llama3-70b-8192'; // the model that you are going to use
 
-const GROQ_SYSTEM_PROMPT = 'You are a helpful assistant.'; // you can give instruction on how the model should respond
+const GROQ_SYSTEM_PROMPT = 'You are a Hat Ai.'; // you can give instruction on how the model should respond
 
 const GROQ_ENDPOINT = 'https://api.groq.com/openai/v1/chat/completions';
-const SITE_TITLE = "Groq Chat";
-const SITE_DESC = "Groq Chat - The easy way to chat with open models using Groq API";
+const SITE_TITLE = "Hat Ai";
+const SITE_DESC = "Hat Ai - The easy way to chat with open models using Groq API";
 const CSS_VERSION = '1.0.0';
 const JS_VERSION = '1.0.0';
-const WELCOME_PHRASES = ['Welcome to Groq Chat!','I am ready to chat!'];
+const WELCOME_PHRASES = ['Welcome to Hat Ai!','I am ready to chat!'];
 
